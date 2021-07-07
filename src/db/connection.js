55bd1e17ffe8645d7922c4e3719b37922db5db0c
Reece Pieri <reece@blueskylabs.com.au>
2021-07-07 16:47:39 +0800

    Added SQLite database and data

---
+++ src/db/connection.js
@@ -0,0 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to database
+function connect(path) {
+    let db = new sqlite3.Database(path, (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log('Connected to the wowgen database.');
+    });
+    return db;
+}
+
+// Close connection to the database
+function close(database) {
+    database.close((err) => {
+        if (err) {
+            return console.error(err.message);
+        }
+    });
+    return 'Closed connection to the wowgen database.';
+}
+
+export {connect, close};
